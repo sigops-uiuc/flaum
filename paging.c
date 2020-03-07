ebf493c1b6d508f7743f5af82a2883c248b594dc
Chinmaya Krishnan Mahesh <chinmaya.mahesh@disroot.org>
2020-03-07 13:43:12 -0600

    add alloc and free page

--- paging.c
+++ paging.c
@@ -4,7 +4,7 @@ extern uint8_t __end;
 
 // The size of this array should be determined by the amount of memory the hardware actually has. (total_mem/page_size)
 ///@todo declare somewhere else?
-page_flags_t page_table[1024];
+page_flags_t page_table[PAGE_NUM_TMP];
 
 void init_pages(){
     int i;
@@ -15,11 +15,27 @@ void init_pages(){
     for(i = 0; i < kernel_pages; i++){
         page_table[i].allocated = 1;
         page_table[i].kernel = 1;
+	page_table[i].mapped_addr = 4096 * i;
     }
     for(; i < PAGE_NUM_TMP; i++){
         page_table[i].allocated = 0;
         page_table[i].kernel = 0;
+	page_table[i].mapped_addr = 4096 * i;
     }
 
     return;
+}
+
+void *alloc_page() {
+	for (int i = 0; i < PAGE_NUM_TMP; i++) {
+		if (page_table[i].allocated == 0) {
+			return (void *) page_table[i].mapped_addr;
+		}
+	}
+	return (void *) ERR_NO_PAGE;
+}
+
+void free_page(void *ptr) {
+	int mapped_page = (int)ptr / PAGE_SIZE;
+	page_table[mapped_page].allocated = 0;
 }
